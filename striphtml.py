8c204706eee2a1770c50fb8ad8dc3f3d3c73a4dd
Tony Rogers <tony.rogers@rackspace.com>
2015-05-28 20:41:59 -0500

    removing some shitty css too

--- striphtml.py
+++ striphtml.py
@@ -2,14 +2,14 @@ from HTMLParser import HTMLParser
 import csv
 import re
 import sys
- 
- 
+
+
 if len(sys.argv) < 3:
     print 'Usage: python {0} <file-in> <file-out>'.format(sys.argv[0])
- 
+
 filein = sys.argv[1]
 fileout = sys.argv[2]
- 
+
 class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
@@ -18,21 +18,22 @@ class MLStripper(HTMLParser):
         self.fed.append(d)
     def get_data(self):
         return ''.join(self.fed)
- 
+
 def strip_tags(html):
     s = MLStripper()
     s.feed(html)
     return s.get_data()
- 
- 
+
+
 allthethings = []
 with open(filein, 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
-        row[2] = re.sub('[ \n]{3,40}', '\n', strip_tags(row[2]))
+        row[2] = re.sub('.*\}\n', '', re.sub('[ \n]{3,40}', '\n', strip_tags(row[2])))
         allthethings.append(row)
- 
+
 with open(fileout, 'wb') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     for row in allthethings:
         writer.writerow(row)
+
